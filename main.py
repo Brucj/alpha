3ec7d8ac857fdb7469983a59425480425ce65004
Brucj <165475989+Brucj@users.noreply.github.com>
2025-07-24 00:48:51 +0300

    Update main.py
    a little updates

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
 ﻿print('Hello GitHub!')
 print('This is a Python script.')
 print('It is running successfully.')
-import time
-def countdown(seconds):
-    while seconds > 0:
-        print(f"Time left: {seconds}s", end="\r")
-        time.sleep(1)
-        seconds -= 1
-    print("Time's up! Stop procrastinating!")
-countdown(60)  # 1-minute timer
